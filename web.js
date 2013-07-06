93b55769c2977ee74fb2e333a93d120e027400a9
haleboppp <haleboppp@ubuntu.ubuntu-domain>
2013-07-06 23:29:49 +0200

    Pending changes done

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2 (two)!');
+  var buffer = fs.readFileSync('index.html');
+//  response.send('Hello World 2 (two)!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
